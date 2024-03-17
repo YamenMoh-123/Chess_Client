5318f1d693c8cb9fab732962843ccb1cf4d5d38b
Yazan Hailat <yazan.mmh@gmail.com>
2024-03-17 19:18:39 -0400

    added sprites and piece class

--- src/PieceObject.java
+++ src/PieceObject.java
@@ -1,3 +1,12 @@
-public class PieceObject {
+
+public class PieceObject  {
+    int value;
+    String name;
+
+    public PieceObject(int value, String name){
+        this.value = value;
+        this.name = name;
+    }
+
 
 }
---
+++ src/Resources.java
@@ -0,0 +1,7 @@
+
+public class Resources {
+
+    public static SpriteSheet pieceSheet = new SpriteSheet("Resources/Sprites/Pieces_Sprites.png", 2, 6, 133, 133);
+
+
+}
