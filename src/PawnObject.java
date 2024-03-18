9fe316511ee56b2adaf656fbbd23c655afbe11eb
user <mohieddy@uwindsor.ca>
2024-03-17 21:15:46 -0400

    movement logic

--- src/PawnObject.java
+++ src/PawnObject.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.util.ArrayList;
 
 public class PawnObject extends PieceObject{
 
@@ -6,6 +7,27 @@ public class PawnObject extends PieceObject{
         super(value, name, color);
     }
 
-
+    @Override
+    public ArrayList<String> validMoves(String startingPos){
+        ArrayList<String> validMoves = new ArrayList<String>();
+        int x = startingPos.charAt(0) - 97;
+        int y = startingPos.charAt(2) - 49;
+        if(this.color == Color.WHITE){
+            if(y == 1){
+                validMoves.add((char)(x+97) + " " + (y+2));
+            }
+            if(y+1 < 8){
+                validMoves.add((char)(x+97) + " " + (y+2));
+            }
+        }else{
+            if(y == 6){
+                validMoves.add((char)(x+97) + " " + (y-2));
+            }
+            if(y-1 >= 0){
+                validMoves.add((char)(x+97) + " " + (y-2));
+            }
+        }
+        return validMoves;
+    }
 
 }
