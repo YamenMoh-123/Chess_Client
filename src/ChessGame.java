c2896526c642b263ce00550540fbc3e0bf1478ea
Yazan Hailat <yazan.mmh@gmail.com>
2024-04-08 19:13:27 -0400

    added comments

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -21,20 +21,31 @@ public class ChessGame extends JPanel {
                     // Listen for notifications from the server
                     notification = ChessGame.fromServer.readLine();
                     if (notification != null) {
-                        // Logic to handle the notification
-                        // For example, you can update the game state, display messages to the user, etc.
+                        // check if the notifcation is a mate or draw, if so end the game
                         System.out.println("Received notification from server: " + notification);
-                        String[] parts = notification.split(" ");
-                        if (notification.length() > 10) {
-                            int oldX = parts[0].charAt(0) - 97;
-                            int oldY = 7 - (parts[1].charAt(0) - 49);
-                            int x = parts[2].charAt(0) - 97;
-                            int y = 7 - (parts[3].charAt(0) - 49);
-                            String piece = parts[4];
-                            boolean enPassant = Boolean.parseBoolean(parts[5]);
-                            boolean enPassantHappened = Boolean.parseBoolean(parts[6]);
-                            ChessBoard.moveResponse(oldX, oldY, x, y, piece, enPassant, enPassantHappened);
-                            Resources.playSound("Resources/Sounds/move-self.wav");
+                        if(notification.equals("Mate")) {
+                            JOptionPane.showMessageDialog(null, "Checkmate! You win!", "Notice", JOptionPane.INFORMATION_MESSAGE);
+                            System.exit(0); // Close the application
+                        }
+                        else if(notification.equals("Draw")) {
+                            JOptionPane.showMessageDialog(null, "Draw! No winner.", "Notice", JOptionPane.INFORMATION_MESSAGE);
+                            System.exit(0); // Close the application
+                        }
+                        else {
+                            // Parse the notification and update the game state
+                            String[] parts = notification.split(" ");
+                            if (notification.length() > 10) {
+                                int oldX = parts[0].charAt(0) - 97;
+                                int oldY = 7 - (parts[1].charAt(0) - 49);
+                                int x = parts[2].charAt(0) - 97;
+                                int y = 7 - (parts[3].charAt(0) - 49);
+                                String piece = parts[4];
+                                boolean enPassant = Boolean.parseBoolean(parts[5]);
+                                boolean enPassantHappened = Boolean.parseBoolean(parts[6]);
+                                ChessBoard.moveResponse(oldX, oldY, x, y, piece, enPassant, enPassantHappened);
+                                Resources.playSound("Resources/Sounds/move-self.wav");
+
+                            }
                         }
                     }
                 }
@@ -47,20 +58,25 @@ public class ChessGame extends JPanel {
 
     public static void main(String[] args) {
         try{
-            JTextField textField = new JTextField(20); // Create a text field
+            // Create a text field to get the server IP address
+            JTextField textField = new JTextField(20);
             JPanel panel = new JPanel();
-            panel.add(new JLabel("Enter the address:"));
+            panel.add(new JLabel("Enter the Serer IP Address:"));
             panel.add(textField);
             int result = JOptionPane.showConfirmDialog(null, panel, "Enter Name", JOptionPane.OK_CANCEL_OPTION);
             String userInput = null;
             if (result == JOptionPane.OK_OPTION) {
-                userInput = textField.getText(); // Get text from the text field
+                // Get text from the text field
+                userInput = textField.getText();
                 if (!userInput.isEmpty()) {
+                    // Convert the user input to an InetAddress object
                     InetAddress serverAddress = InetAddress.getByName(userInput);
                 }
             }
+            // Create a socket connection to the server
+            InetAddress serverAddress = InetAddress.getByName(userInput);
             int serverPort = 1234;
-            socket = new Socket(InetAddress.getByName(userInput), serverPort);
+            socket = new Socket(serverAddress, serverPort);
             fromServer = new BufferedReader(new InputStreamReader(ChessGame.socket.getInputStream()));
         } catch (
                 IOException ioException) {
